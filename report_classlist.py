3771bab6227f7873c2498efd01ab6e39f41764d9
Alex Deweert <alexand@dblinux3.csc.uvic.ca>
2018-04-09 02:08:47 -0700

    Finished last bits

--- report_classlist.py
+++ report_classlist.py
@@ -1,9 +1,16 @@
 # report_classlist.py
-# CSC 370 - Spring 2018 - Starter code for Assignment 4
-#
-#
+# CSC 370 - Spring 2018
+# Alex L. DEWEERT
+# V00855767 CSC 370 - Spring 2018 - Starter code for Assignment 4
 
 import psycopg2, sys
+
+try:
+	conn = psycopg2.connect( dbname="alexand", user="alexand", host="studdb1.csc.uvic.ca", password="V00855767")
+except:
+	print("Error: Unable to connect to the database!")
+
+cur = conn.cursor()
 
 def print_header(course_code, course_name, term, instructor_name):
 	print("Class list for %s (%s)"%(str(course_code), str(course_name)) )
@@ -20,26 +27,45 @@ def print_footer(total_enrolled, max_capacity):
 	print("%s/%s students enrolled"%(str(total_enrolled),str(max_capacity)) )
 
 
-''' The lines below would be helpful in your solution
+#The lines below would be helpful in your solution
 if len(sys.argv) < 3:
 	print('Usage: %s <course code> <term>'%sys.argv[0], file=sys.stderr)
 	sys.exit(0)
 	
 course_code, term = sys.argv[1:3]
-'''
 
+try:
+	cur.execute(""" 
+	with a as ( select * from enrollments natural join course_offerings
+	where course_id = '{}' and term_code = {} ),
+	b as ( select course_id, name, term_code, instructor, student_id, grade from a ),
+	c as ( select course_id, course_offerings.name as course_name, term_code, instructor, b.student_id, grade, capacity, students.name
+	from b natural join course_offerings cross join students where b.student_id = students.student_id)
+	select course_id, course_name, term_code, instructor, student_id, grade, capacity, name from c
+	;""".format(course_code,term))
+except:
+	print("Error")
+
+rows = []
+rows = cur.fetchall()
+
+#for a in rows:
+#	print(a)
 
 # Mockup: Print a class list for CSC 370
-course_code = 'CSC 370'
-course_name = 'Database Systems'
-course_term = 201801
-instructor_name = 'Bill Bird'
+course_name = rows[0][1]
+course_term = term
+instructor_name = rows[0][3]
 print_header(course_code, course_name, course_term, instructor_name)
 
+count=0
 #Print records for a few students
-print_row('V00123456', 'Rebecca Raspberry', 81)
-print_row('V00123457', 'Alissa Aubergine', 90)
-print_row('V00123458', 'Neal Naranja', 83)
+for a in rows:
+#print_row('V00123456', 'Rebecca Raspberry', 81)
+	print_row(a[4],a[7],a[5])
+	count = count+1
 
 #Print the last line (enrollment/max_capacity)
-print_footer(3,150)
+print_footer(count,rows[0][6])
+cur.close()
+conn.close()
--- report_transcript.py
+++ report_transcript.py
@@ -1,10 +1,41 @@
 # report_transcript.py
-# CSC 370 - Spring 2018 - Starter code for Assignment 4
-#
+# CSC 370 - Spring 2018
+# Alex L. DEWEERT
+# V00855767
 
 import psycopg2, sys
 
 
+
+try:
+	conn = psycopg2.connect( dbname="alexand", user="alexand", host="studdb1.csc.uvic.ca", password="V00855767")
+	#print("Connected!")
+except:
+	print("Error: Unable to connect to the database!")
+
+cur = conn.cursor()
+
+
+def print_row(term, course_code, course_name, instructor_name, total_enrollment, maximum_capacity):
+	print("%6s %10s %-35s %-25s %s/%s"%(str(term), str(course_code), str(course_name), str(instructor_name), str(total_enrollment), str(maximum_capacity)) )
+
+
+#The lines below would be helpful in your solution
+if len(sys.argv) < 2:
+	print('Usage: %s <student id>'%sys.argv[0], file=sys.stderr)
+	sys.exit(0)
+	
+student_id = sys.argv[1]
+
+# Mockup: Print some data for a few made up classes
+cur.execute("""  
+with a as ( select term_code, course_id, grade, student_id from enrollments where student_id = '{}'),
+b as ( select term_code, course_id, name as course_name, grade, student_id from a natural join course_offerings order by term_code, course_id),
+c as ( select * from b cross join students where b.student_id = students.student_id )
+select term_code, course_id, course_name, grade, name from c
+""".format(student_id))
+rows = []
+rows = cur.fetchall()
 
 def print_header(student_id, student_name):
 	print("Transcript for %s (%s)"%(str(student_id), str(student_name)) )
@@ -15,21 +46,16 @@ def print_row(course_term, course_code, course_name, grade):
 	else:
 		print("%6s %10s %-35s   (NO GRADE ASSIGNED)"%(str(course_term), str(course_code), str(course_name)) )
 
-''' The lines below would be helpful in your solution
-if len(sys.argv) < 2:
-	print('Usage: %s <student id>'%sys.argv[0], file=sys.stderr)
-	sys.exit(0)
-	
-student_id = sys.argv[1]
-'''
-
 
 # Mockup: Print a transcript for V00123456 (Rebecca Raspberry)
-student_id = 'V00123456'
-student_name = 'Rebecca Raspberry'
+student_name = rows[0][4]
 print_header(student_id, student_name)
 
+for r in rows:
+	print_row( r[0],r[1],r[2],r[3] )
+#print_row(201709,'CSC 110','Fundamentals of Programming: I', 90)
+#print_row(201709,'CSC 187','Recursive Algorithm Design', None) #The special value None is used to indicate that no grade is assigned.
+#print_row(201801,'CSC 115','Fundamentals of Programming: II', 75)
 
-print_row(201709,'CSC 110','Fundamentals of Programming: I', 90)
-print_row(201709,'CSC 187','Recursive Algorithm Design', None) #The special value None is used to indicate that no grade is assigned.
-print_row(201801,'CSC 115','Fundamentals of Programming: II', 75)
+cur.close()
+conn.close()
